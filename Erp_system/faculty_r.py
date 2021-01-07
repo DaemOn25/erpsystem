320cd5b70ee3eb7e8201ff9720db443fb717989b
DaemOn25 <tarunbisht252000@gmail.com>
2021-01-07 12:50:03 +0530

    update

--- Erp_system/faculty_r.py
+++ Erp_system/faculty_r.py
@@ -1,83 +1,95 @@
-import tkinter 
+import tkinter
 import sqlite3
 import tkinter.messagebox
 
-conn = sqlite3.connect("F:/tArUN/programs/Python programs/erpsystem/Erp_system/Database/erp.db")
+conn = sqlite3.connect(
+    "erp.db")
 c = conn.cursor()
 
-class Database:
-    def __init__(self , master , *args , **kwargs):
 
-        self.master= master
+class Database:
+    def __init__(self, master, *args, **kwargs):
+
+        self.master = master
         # heading label
-        self.heading = tkinter.Label(master, text="Faculty Registration",  fg='steelblue', font=('arial 40 bold'))
+        self.heading = tkinter.Label(
+            master, text="Faculty Registration",  fg='steelblue', font=('arial 40 bold'))
         self.heading.place(x=450, y=0)
-        
-        #labels 
-        self.roll_l = tkinter.Label(master, text="Enter Faculty's Roll Number", font=('arial 18 bold'))
+
+        # labels
+        self.roll_l = tkinter.Label(
+            master, text="Enter Faculty's Roll Number", font=('arial 18 bold'))
         self.roll_l.place(x=0, y=100)
 
-        self.name_l = tkinter.Label(master, text="Enter Faculty's Name", font=('arial 18 bold'))
+        self.name_l = tkinter.Label(
+            master, text="Enter Faculty's Name", font=('arial 18 bold'))
         self.name_l.place(x=0, y=200)
 
-        self.sub_l = tkinter.Label(master, text="Enter Faculty's Subject", font=('arial 18 bold'))
+        self.sub_l = tkinter.Label(
+            master, text="Enter Faculty's Subject", font=('arial 18 bold'))
         self.sub_l.place(x=0, y=300)
 
-        self.phone_l = tkinter.Label(master, text="Enter Faculty's Phone", font=('arial 18 bold'))
+        self.phone_l = tkinter.Label(
+            master, text="Enter Faculty's Phone", font=('arial 18 bold'))
         self.phone_l.place(x=0, y=400)
 
-        #entries for labels
+        # entries for labels
         self.roll_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.roll_e.place(x=380 , y=100)
+        self.roll_e.place(x=380, y=100)
 
         self.name_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.name_e.place(x=380 , y=200)
+        self.name_e.place(x=380, y=200)
 
         self.sub_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.sub_e.place(x=380 , y=300)
+        self.sub_e.place(x=380, y=300)
 
         self.phone_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.phone_e.place(x=380 , y=400)
+        self.phone_e.place(x=380, y=400)
 
-         # add to db button
-        self.btn_add = tkinter.Button(master, text="ADD", width=20, height=2, bg='steelblue', command = self.write_fac)
+        # add to db button
+        self.btn_add = tkinter.Button(
+            master, text="ADD", width=20, height=2, bg='steelblue', command=self.write_fac)
         self.btn_add.place(x=585, y=550)
 
-         # clear field button
-        self.btn_clr = tkinter.Button(master, text="CLEAR", width=20, height=2, bg='steelblue', command = self.clear_all)
+        # clear field button
+        self.btn_clr = tkinter.Button(
+            master, text="CLEAR", width=20, height=2, bg='steelblue', command=self.clear_all)
         self.btn_clr.place(x=400, y=550)
 
         # text box for logs
-        self.tBox = tkinter.Text(master , width = 60, height = 27)
-        self.tBox.place(x=850 ,  y= 100)
-    
-    def write_fac(self , *args , **kwargs):
-        #get from entries
+        self.tBox = tkinter.Text(master, width=60, height=27)
+        self.tBox.place(x=850,  y=100)
+
+    def write_fac(self, *args, **kwargs):
+        # get from entries
         self.roll = self.roll_e.get()
-        self.name = self.name_e.get()      
+        self.name = self.name_e.get()
         self.sub = self.sub_e.get()
         self.phone = self.phone_e.get()
 
-        if self.name == '' or  self.roll == '' or self.sub == '' or self.phone == '' :
-            tkinter.messagebox.showwarning("Warning" , "PLEASE FILL ALL THE ENTRIES")
+        if self.name == '' or self.roll == '' or self.sub == '' or self.phone == '':
+            tkinter.messagebox.showwarning(
+                "Warning", "PLEASE FILL ALL THE ENTRIES")
         else:
-            
+
             sql = "INSERT INTO teacher ( id , name , subject , phone) VALUES(?,?,?,?)"
-            c.execute(sql , (self.roll , self.name , self.sub , self.phone))
+            c.execute(sql, (self.roll, self.name, self.sub, self.phone))
             conn.commit()
-            #text box msg
-            self.tBox.insert(tkinter.END, "\n\n Inserted " + str(self.name) + " into the database\n with roll id = " + str(self.roll_e.get()))
+            # text box msg
+            self.tBox.insert(tkinter.END, "\n\n Inserted " + str(self.name) +
+                             " into the database\n with roll id = " + str(self.roll_e.get()))
 
-            tkinter.messagebox.showinfo("Success" , "Successfully added to the database")
-    
-    def clear_all(self, *args , **kwargs):
-        #clear the entry fields
-        self.roll_e.delete(0 , tkinter.END)
-        self.name_e.delete(0 , tkinter.END)
-        self.sub_e.delete(0 , tkinter.END)
-        self.phone_e.delete(0 , tkinter.END)
+            tkinter.messagebox.showinfo(
+                "Success", "Successfully added to the database")
 
-  
+    def clear_all(self, *args, **kwargs):
+        # clear the entry fields
+        self.roll_e.delete(0, tkinter.END)
+        self.name_e.delete(0, tkinter.END)
+        self.sub_e.delete(0, tkinter.END)
+        self.phone_e.delete(0, tkinter.END)
+
+
 # creating the object
 root = tkinter.Tk()
 b = Database(root)
--- Erp_system/student_l.py
+++ Erp_system/student_l.py
@@ -1,69 +1,79 @@
-import tkinter 
+import tkinter
 import sqlite3
 import tkinter.messagebox
 
-conn = sqlite3.connect("F:/tArUN/programs/Python programs/erpsystem/Erp_system/Database/erp.db")
+conn = sqlite3.connect(
+    "erp.db")
 c = conn.cursor()
 
-class Database:
-    def __init__(self , master , *args , **kwargs):
 
-        self.master= master
+class Database:
+    def __init__(self, master, *args, **kwargs):
+
+        self.master = master
         # heading label
-        self.heading = tkinter.Label(master, text="Student Information",  fg='steelblue', font=('arial 40 bold'))
+        self.heading = tkinter.Label(
+            master, text="Student Information",  fg='steelblue', font=('arial 40 bold'))
         self.heading.place(x=300, y=0)
-        
-        #labels 
-        self.search_l = tkinter.Label(master, text="Enter Student's Roll Number ", font=('arial 18 bold'))
+
+        # labels
+        self.search_l = tkinter.Label(
+            master, text="Enter Student's Roll Number ", font=('arial 18 bold'))
         self.search_l.place(x=0, y=100)
 
-        self.roll_l = tkinter.Label(master, text=" Roll Number", font=('arial 18 bold'))
+        self.roll_l = tkinter.Label(
+            master, text=" Roll Number", font=('arial 18 bold'))
         self.roll_l.place(x=0, y=200)
 
-        self.name_l = tkinter.Label(master, text=" Name", font=('arial 18 bold'))
+        self.name_l = tkinter.Label(
+            master, text=" Name", font=('arial 18 bold'))
         self.name_l.place(x=0, y=300)
 
-        self.course_l = tkinter.Label(master, text=" Course", font=('arial 18 bold'))
+        self.course_l = tkinter.Label(
+            master, text=" Course", font=('arial 18 bold'))
         self.course_l.place(x=0, y=400)
 
-        self.branch_l = tkinter.Label(master, text=" Branch", font=('arial 18 bold'))
+        self.branch_l = tkinter.Label(
+            master, text=" Branch", font=('arial 18 bold'))
         self.branch_l.place(x=0, y=500)
 
-        self.attend_l = tkinter.Label(master, text=" Attendance", font=('arial 18 bold'))
+        self.attend_l = tkinter.Label(
+            master, text=" Attendance", font=('arial 18 bold'))
         self.attend_l.place(x=0, y=600)
 
-        #entries for labels
+        # entries for labels
         self.search_e = tkinter.Entry(master, width=15, font=('arial 18 bold'))
-        self.search_e.place(x=380 , y=100)
+        self.search_e.place(x=380, y=100)
 
         self.roll_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.roll_e.place(x=200 , y=200)
+        self.roll_e.place(x=200, y=200)
 
         self.name_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.name_e.place(x=200 , y=300)
+        self.name_e.place(x=200, y=300)
 
         self.course_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.course_e.place(x=200 , y=400)
+        self.course_e.place(x=200, y=400)
 
         self.branch_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.branch_e.place(x=200 , y=500)
+        self.branch_e.place(x=200, y=500)
 
         self.attend_e = tkinter.Entry(master, width=30, font=('arial 18 bold'))
-        self.attend_e.place(x=200 , y=600)
+        self.attend_e.place(x=200, y=600)
 
-         #search db button
-        self.btn_search = tkinter.Button(master, text="Search", width=20, height=2, bg='steelblue', command = self.search)
+        # search db button
+        self.btn_search = tkinter.Button(
+            master, text="Search", width=20, height=2, bg='steelblue', command=self.search)
         self.btn_search.place(x=600, y=100)
 
     def search(self, *args, **kwargs):
         sql = "SELECT * FROM student WHERE id=?"
         result = c.execute(sql, (self.search_e.get(), ))
         for r in result:
-            self.n1 = r[0] #roll
-            self.n2 = r[1] #name
-            self.n3 = r[2] #branch
-            self.n4 = r[3] #course
-            self.n5 = r[4] #attendance
+            self.n1 = r[0]  # roll
+            self.n2 = r[1]  # name
+            self.n3 = r[2]  # branch
+            self.n4 = r[3]  # course
+            self.n5 = r[4]  # attendance
         conn.commit()
 
         # insert into the entries to update
@@ -92,7 +102,7 @@ class Database:
         self.attend_e.insert(0, str(self.n5))
         self.attend_e.config(state='readonly')
 
-  
+
 # creating the object
 root = tkinter.Tk()
 b = Database(root)
